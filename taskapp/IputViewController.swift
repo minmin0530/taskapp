7a4c214134474379d9670304773661aa68373a87
izumi_yoshiki <izumiyoshiki@icloud.com>
2019-01-20 12:51:30 +0900

    タスクアプリ課題

---
+++ taskapp/IputViewController.swift
@@ -0,0 +1,33 @@
+//
+//  IputViewController.swift
+//  taskapp
+//
+//  Created by Yoshiki Izumi on 2019/01/20.
+//  Copyright © 2019 Yoshiki Izumi. All rights reserved.
+//
+
+import UIKit
+
+class IputViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
